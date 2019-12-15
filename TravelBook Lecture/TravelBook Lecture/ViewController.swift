5d768ae3f7bbb9954ee9a971ecb4a341ed3d04d9
Murat Han Acıpayam <acipayammurathan@gmail.com>
2019-12-15 19:56:23 +0300

    Add files via upload

---
+++ TravelBook Lecture/TravelBook Lecture/ViewController.swift
@@ -0,0 +1,156 @@
+//
+//  ViewController.swift
+//  TravelBook Lecture
+//
+//  Created by Murat Han on 14.12.2019.
+//  Copyright © 2019 Murat Han. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import CoreData
+
+class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDelegate {
+
+    
+    @IBOutlet weak var commentText: UITextField!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    @IBOutlet weak var nameText: UITextField!
+    
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
+    
+    var selectedTitle = ""
+    var selectedTitleID : UUID?
+    
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var annotationLatitude = Double()
+    var annotationLongitude = Double()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization() // We are asking permisson when in use
+        locationManager.startUpdatingLocation()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3 //if 3 seconds longer press then it is pinned on map
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
+        if selectedTitle != "" {
+            //CoreData
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            let idString = selectedTitleID!.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject]{
+                        
+                        if let title = result.value(forKey: "title") as? String{
+                            annotationTitle = title
+                            
+                            if let subtitle = result.value(forKey: "subtitle") as? String{
+                                annotationSubtitle = subtitle
+                                
+                                if let latitude = result.value(forKey: "latitude") as? Double{
+                                    annotationLatitude = latitude
+                                    
+                                    if let longitude = result.value(forKey: "longitude") as? Double{
+                                        annotationLongitude = longitude
+                                        
+                                        let annotation = MKPointAnnotation()
+                                        annotation.title = annotationTitle
+                                        annotation.subtitle = annotationSubtitle
+                                        let coordinate = CLLocationCoordinate2D(latitude: annotationLatitude, longitude: annotationLongitude)
+                                        annotation.coordinate = coordinate
+                                        
+                                        mapView.addAnnotation(annotation)
+                                        nameText.text = annotationTitle
+                                        commentText.text = annotationSubtitle
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("error")
+                
+            }
+            
+            
+        } else {
+            // Add new data
+        }
+        
+        
+    }
+    
+    
+    @objc func chooseLocation(gestureRecognizer : UILongPressGestureRecognizer){
+        if gestureRecognizer.state == .began {
+            
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            chosenLatitude = touchedCoordinates.latitude
+            chosenLongitude = touchedCoordinates.longitude
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinates
+            annotation.title = nameText.text
+            annotation.subtitle = commentText.text
+            self.mapView.addAnnotation(annotation)
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)//We have taken users last location
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        
+        newPlace.setValue(nameText.text, forKey: "title")
+        newPlace.setValue(commentText.text, forKey: "subtitle")
+        newPlace.setValue(chosenLatitude, forKey: "latitude")
+        newPlace.setValue(chosenLongitude, forKey: "longitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+            
+        } catch {
+            print("error")
+        }
+        
+        
+    }
+    
+}
+
